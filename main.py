6aeb1d93f9be5229a6c209133b26081a02ce63cb
Kenshi Kadarusman <kenshikadarusman@gmail.com>
2026-03-05 08:08:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-import cv2 as cv
 def main():
-    # Open default camera
-    cap = cv.VideoCapture(0)
-
-    while True:
-        ret, frame = cap.read()
-        if not ret: break
-
-        # Display frame
-        cv.imshow('Camera Feed', frame)
-
-        # Press 'q' to exit
-        if cv.waitKey(1) == ord('q'):
-            break
-
-    cap.release()
-    cv.destroyAllWindows()
+    print("Hello From HealthTech!")
 
 
 if __name__ == "__main__":
