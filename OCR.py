40287a99652df166f6cb343aed725181185cbd66
Kenshi Kadarusman <kenshikadarusman@gmail.com>
2026-03-30 20:55:42 -0400

    test speed

--- OCR.py
+++ OCR.py
@@ -4,6 +4,11 @@ def main():
     print("Hello From HealthTech!")
     reader = easyocr.Reader(['en'], gpu=False)
     result = reader.readtext('TestImage/indoorSigns.jpg', detail=0, paragraph=True)
+    print(result)
+    result = reader.readtext('TestImage/signs.jpg', detail=0, paragraph=True)
+    print(result)
+    result = reader.readtext('TestImage/name.jpg', detail=0, paragraph=True)
+    print(result)
 
 
 if __name__ == "__main__":
